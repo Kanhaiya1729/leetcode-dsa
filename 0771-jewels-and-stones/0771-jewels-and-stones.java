6054a389a7d9126f4e28efcf788548baae7ec404
KANHAIYA SAHANI <kanhaiya.sahani2019@gmail.com>
2026-08-28 12:21:09 +0530

    Time: 1 ms (60.87%), Space: 43.3 MB (20.28%) - LeetHub

---
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        HashMap<Character,Integer>map=new HashMap<>();
+        for(int i=0;i<jewels.length();i++){
+            map.put(jewels.charAt(i),i);
+        }
+        int count=0;
+        for(int i=0;i<stones.length();i++){
+            if(map.containsKey(stones.charAt(i))){
+                count++;
+            }
+
+        }
+        return count;
+    }
+}
