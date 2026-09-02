1b5d39c07919f606f8c6faa0d773db007ca3c68e
KANHAIYA SAHANI <kanhaiya.sahani2019@gmail.com>
2026-09-02 12:09:26 +0530

    Time: 5 ms (12.05%), Space: 48.6 MB (75.41%) - LeetHub

--- 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.java
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.java
@@ -1,38 +1,32 @@
 class Solution {
-    public int maxSatisfied(int[] customers, int[] grumpy, int minutes){
+    public int maxSatisfied(int[] customers, int[] grumpy, int minutes) {
         int n=customers.length;
-        int size=minutes;
-        int left=0;
-        int WindowSum=0;
-        int sum=0;
         int max=0;
+        int sum=0;
+        int WindowSum=0;
+        int left=0;
+        int size=minutes;
         for(int right=0;right<n;right++){
-            if(grumpy[right]==1){
-                WindowSum+=customers[right];
-            }
             if(grumpy[right]==0){
                 sum+=customers[right];
+            }else{
+                WindowSum+=customers[right];
+                
             }
-
 
             if(right-left+1 > size){
                 if(grumpy[left]==1){
                     WindowSum-=customers[left];
-
                 }
-                   left++;
+                left++;
             }
-            if(right-left+1==size ){
+            if(right-left+1 == size){
                 max=Math.max(max,WindowSum);
-
             }
-
+            
 
         }
-        return max+sum;
+        return sum+max;
         
-
-    
- 
     }
 }
