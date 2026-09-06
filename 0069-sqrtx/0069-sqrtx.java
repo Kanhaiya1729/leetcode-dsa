a71bbfc784bdc44e115f57daac0340e6a6b543fc
KANHAIYA SAHANI <kanhaiya.sahani2019@gmail.com>
2026-09-06 11:58:30 +0530

    Time: 1 ms (98.87%), Space: 42.7 MB (42.96%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -7,12 +7,12 @@ class Solution {
         int e=x;
         int ans=0;
           while(s<=e){
-            int mid= (s+e)/2;
-            if(mid<=x/mid){
-                ans=mid;
-                s=mid+1;
+            long mid= s+(e-s)/2;
+            if(mid*mid<=x){
+                ans=(int)mid;
+                s=(int)mid+1;
             }else{
-                e=mid-1;
+                e=(int)mid-1;
             }      
     }
      return ans;
