8364d561d88266420db6ef3872e0745f95604a91
KANHAIYA SAHANI <kanhaiya.sahani2019@gmail.com>
2026-09-07 10:48:18 +0530

    Time: 0 ms (100%), Space: 80.2 MB (76.94%) - LeetHub

---
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int n=arr.length;
+        int s=0;
+        int e=n-1;
+
+        while(s<=e){
+            int mid=s+(e-s)/2;
+            if(arr[mid]>=arr[mid+1]){
+                e=mid-1;
+            }else{
+               s = mid+1;;
+            }
+        }
+        return s;
+
+
+    }
+}
