e758eed0323a3fa1d1515c16aad8f9b6643abbbe
KANHAIYA SAHANI <kanhaiya.sahani2019@gmail.com>
2026-09-09 09:55:07 +0530

    Time: 5 ms (36.98%), Space: 43.3 MB (83.27%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
@@ -1,5 +1,8 @@
 class Solution {
     public String removeOuterParentheses(String s) {
+        if(s.length()==1 || s.length()==2 ){
+            return "";
+        }
         int depth=0;
         StringBuilder ans=new StringBuilder();
         for(int i=0;i<s.length();i++){
