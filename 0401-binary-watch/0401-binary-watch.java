f7327c0ff5b8d252ed19403e143acf277c05b721
KANHAIYA SAHANI <kanhaiya.sahani2019@gmail.com>
2026-09-13 10:20:22 +0530

    Time: 9 ms (39.05%), Space: 44.1 MB (68.28%) - LeetHub

--- 0401-binary-watch/0401-binary-watch.java
+++ 0401-binary-watch/0401-binary-watch.java
@@ -1,8 +1,6 @@
 class Solution {
     public List<String> readBinaryWatch(int turnedOn) {
         List<String>ans=new ArrayList<>();
-        int Hr[]=new int[4];
-        int Min[]=new int[6];
         
       for(int hr=0;hr<12;hr++){
         for(int min=0;min<60;min++){
