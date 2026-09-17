ac4eecd79fd1e7b4842f90e82df6351474f519cd
KANHAIYA SAHANI <kanhaiya.sahani2019@gmail.com>
2026-09-17 12:02:36 +0530

    Time: 8 ms (73.64%), Space: 47.2 MB (32.93%) - LeetHub

---
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minDeletionSize(String[] strs) {
+        int count=0;
+        for(int col=0;col<strs[0].length();col++){
+        for(int row=0;row<strs.length-1;row++){
+           if(strs[row].charAt(col)>strs[row+1].charAt(col)){
+            count++;
+            break;
+           
+           }
+
+            }
+        }
+        return count;
+        }
+        
+    }
