4dc707469deeb4d7cb1021486f4b7d8dc9a6f762
KANHAIYA SAHANI <kanhaiya.sahani2019@gmail.com>
2026-09-19 11:29:40 +0530

    Time: 0 ms (100%), Space: 46.6 MB (14.23%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.java
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.java
@@ -3,12 +3,8 @@ class Solution {
         int n=mat.length;
         int sum=0;
         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(i==j){
-                   sum+=mat[i][j];
-                }
-
-            }
+            sum+=mat[i][i];
+            
         }
         int i=0;
         while(i<n){
@@ -18,10 +14,9 @@ class Solution {
         if(n%2!=0){
         int mid=(0+(n-1))/2;
         sum-=mat[mid][mid];
+        
+        }
         return sum;
-        }else{
-             return sum;
         }
         
     }
-}
