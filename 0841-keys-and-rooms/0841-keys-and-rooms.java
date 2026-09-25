a349e3627208cfbaa5ab3c13620ada5851b58ee0
KANHAIYA SAHANI <kanhaiya.sahani2019@gmail.com>
2026-09-25 11:35:38 +0530

    Time: 1 ms (83.07%), Space: 46.1 MB (80.48%) - LeetHub

---
+++ 0841-keys-and-rooms/0841-keys-and-rooms.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+      boolean vis[]=new boolean[rooms.size()];
+      dfs(rooms,vis,0);
+      for(boolean value: vis){
+        if(!value){
+            return false;
+        }
+      }
+      return true;
+    }
+    public void  dfs(List<List<Integer>> rooms,boolean vis[],int src){
+        vis[src]=true;
+           for(int key: rooms.get(src)){
+            if(!vis[key]){
+                dfs(rooms,vis,key);
+            }
+    }
+}
+}
